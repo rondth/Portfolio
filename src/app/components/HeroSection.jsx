c9629b69eff23499bcb8047d5794dadd6db3c790
rondth <erondathan@gmail.com>
2025-10-18 20:54:03 +0800

    Refactor HeroSection component for clarity

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -48,7 +48,7 @@ const HeroSection = () => {
             <div className="col-span-5 place-self-center mt-4 lg:mt-0">
                 <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                     <Image
-                        src="./images/pfp.png"
+                        src="../images/pfp.png"
                         alt="hero image"
                         className="absolute transform -translate-x-1/2 -translate-y-1/2  top-1/2 left-1/2 rounded-full"
                         width={300}
@@ -63,3 +63,4 @@ const HeroSection = () => {
 
 
 export default HeroSection
+
